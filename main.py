eee5fe1a6fcd23653f01ecefa38e236f3cb905af
Paul MEKHAIL <paul.mekhail4@pm.me>
2025-09-14 08:44:56 +0200

    size modif sub

--- main.py
+++ main.py
@@ -127,4 +127,4 @@ async def bad_request_handler(request: Request, exc):
     return {"message": "Bad request"}
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=5000, workers=1)
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, workers=1)
